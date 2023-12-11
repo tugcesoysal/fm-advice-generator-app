22089a399b121f2ecd3ba1af46991262c76202ee
tugcesoysal <zeyneptugcesoysal@gmail.com>
2023-12-10 20:30:05 -0500

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { adviceData } from "./advice-data";
 
 function App() {
+  const randomNumber = Math.floor(Math.random() * 9) + 1;
+
+  const [advices, setAdvices] = useState(adviceData);
+  const [index, setIndex] = useState(randomNumber);
+  const randomAdvice = advices[index];
+
+  const generateRandomAdvice = () => {
+    const randomNumber = Math.floor(Math.random() * 10);
+    setIndex(randomNumber);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Advice #{randomAdvice.id}</h4>
+      <h1>"{randomAdvice.quote}"</h1>
+      <img
+        src="../images/pattern-divider-mobile.svg"
+        alt="line-break"
+        className="line-break"
+      />
+      <button onClick={generateRandomAdvice}>
+        <img src="../images/icon-dice.svg" alt="icon-dice" />
+      </button>
     </div>
   );
 }
---
+++ src/advice-data.js
@@ -0,0 +1,45 @@
+export const adviceData = [
+  {
+    id: 1,
+    quote:
+      "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.",
+  },
+  {
+    id: 2,
+    quote: "The only way to do great work is to love what you do.",
+  },
+  {
+    id: 3,
+    quote: "Don't watch the clock; do what it does. Keep going.",
+  },
+  {
+    id: 4,
+    quote: "Believe you can and you're halfway there.",
+  },
+  {
+    id: 5,
+    quote: "Your time is limited, don't waste it living someone else's life.",
+  },
+  {
+    id: 6,
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+  },
+  {
+    id: 7,
+    quote: "The best way to predict the future is to create it.",
+  },
+  {
+    id: 8,
+    quote: "Strive not to be a success, but rather to be of value.",
+  },
+  {
+    id: 9,
+    quote: "It always seems impossible until it's done.",
+  },
+  {
+    id: 10,
+    quote:
+      "Don't let the noise of others' opinions drown out your own inner voice.",
+  },
+];
